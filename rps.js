88ee582e1ca0c2b6542f37c6166cdca485e787b3
Jarle Tollaksen <jarlehtollaksen2@live.no>
2022-08-01 20:53:32 +0200

    Add simple scoreboard

--- rps.js
+++ rps.js
@@ -3,15 +3,44 @@ let Computer = 0;
 let PlayerInput;
 let txtBox = document.createElement("p");
 textBox.appendChild(txtBox)
-let choiceTxt = document.getElementById("testi");
-choiceTxt.innerText = "Your choice will be the difference between victory or defeat!"; 
+let choiceTxt = document.getElementById("pChoice");
+choiceTxt.innerText = "Your choice will be the difference between victory or defeat!";
+let cChoiceTxt = document.getElementById("cChoice"); 
+let narrator = document.getElementById("narration");
+narrator.innerText = " WATER BEATS FIRE";
+let pointsP = document.getElementById("playerPoints");
+pointsP.innerText = Player;
+let pointsR = document.getElementById("rivalPoints");
+pointsP.innerText = Computer;
+
+if (Player >= 5 || Computer >=5)
+{
+    if(Player >= 5)
+    {
+        narrator.innerText = "Player wins!"
+        Player = 0;
+        Computer = 0;
+    }
+    else if(Computer >= 5)
+    {
+        narrator.innerText = "Computer wins!"
+        Player = 0;
+        Computer = 0;
+    }
+    else
+    {
+        narrator.innerText = "DRAW!"
+        Player = 0;
+        Computer = 0;
+    }
+}
 
         //Button that chooses rock when clicked
         const buttonROck = document.getElementById("rock")
         buttonROck.addEventListener('click',() => {
         PlayerInput = "rock";
         console.log("rock")
-        choiceTxt.innerText ="You have chosen Rock!";
+        choiceTxt.innerText ="You have chosen FIRE!";
         getComputerChoice();
         playRps(PlayerInput, opponent)})
 
@@ -20,7 +49,7 @@ choiceTxt.innerText = "Your choice will be the difference between victory or def
         buttonScissors.addEventListener('click',() => {
         PlayerInput = "scissors";
         console.log("scissors")
-        choiceTxt.innerText = "You have chosen Scissors!";
+        choiceTxt.innerText = "You have chosen GRASS!";
         getComputerChoice();
         playRps(PlayerInput, opponent)})
 
@@ -29,7 +58,7 @@ choiceTxt.innerText = "Your choice will be the difference between victory or def
         buttonPaper.addEventListener('click',() => {
         PlayerInput = "paper";
         console.log("paper")
-        choiceTxt.innerText ="You have chosen Paper!";
+        choiceTxt.innerText ="You have chosen WATER!";
         getComputerChoice();
         playRps(PlayerInput, opponent)})
 
@@ -53,6 +82,7 @@ function game(){
  }
 }*/
 
+//plays a round of rock paper scissors
 function playRps(PlayerInput, opponent){
  
     if (opponent == "rock")
@@ -60,13 +90,16 @@ function playRps(PlayerInput, opponent){
         switch(PlayerInput)
         {
             case "paper":
+                narrator.innerText = " WATER BEATS FIRE! You win the round";
+
             Player = Player +1;
             console.log(Player)
             break;
             case "scissors":
-                Computer = Computer+ 1; 
+                narrator.innerText = " FIRE BEATS GRASS! You lose the round";
             break;
             default:
+            narrator.innerText = " FIRE vs FIRE! It's a TIE!";
                 Computer = Computer+ 1; 
                 Player = Player +1;            
             break;
@@ -77,12 +110,14 @@ function playRps(PlayerInput, opponent){
     {
         switch(PlayerInput){
          case "rock":
-            Computer = Computer+ 1; 
-                        break;
-        case "scissors": 
+            narrator.innerText = " WATER BEATS FIRE! Rival wins the round";
+            break;
+        case "scissors":
+            narrator.innerText = " GRASS BEATS WATER! You win the round!" 
         Player = Player +1;
                 break;
         default:
+            narrator.innerText = " WATER vs WATER! It's a TIE!";
             Computer = Computer+ 1; 
             Player = Player +1;
         break;
@@ -92,12 +127,16 @@ function playRps(PlayerInput, opponent){
     else if(opponent == "scissors"){
         switch(PlayerInput){
         case "rock":
+            narrator.innerText = " FIRE BEATS GRASS! You win!"
             Player = Player +1;
           break;
         case "paper":
+            narrator.innerText = " GRASS BEATS WATER! RIVAL wins the round!";
+
             Computer = Computer+ 1; 
           break;
             default:
+                narrator.innerText = " GRASS vs GRASS! It's a TIE!";
                 Computer = Computer+ 1; 
                 Player = Player +1;
            break;
@@ -114,15 +153,15 @@ function getComputerChoice(){
     
         if (random == 1){
             opponent = "rock";
-            console.log("opponent has rock");
+            cChoice.innerText = "Rival chooses FIRE";
         }
         else if (random == 2){
             opponent = "paper";
-            console.log("opponent has paper");
+            cChoice.innerText = "Rival chooses WATER";
         }
         else{
             opponent = "scissors";
-            console.log("opponent has scissors");
+            cChoice.innerText = "Rival chooses GRASS";
         }
     
     return opponent;
